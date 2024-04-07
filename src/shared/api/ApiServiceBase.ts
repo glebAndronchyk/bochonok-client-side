53029eb543ba60e9fe12c8b6cdc51e88ddfe7b82
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-04-07 22:35:44 +0300

    fix: tailwind

--- src/shared/api/ApiServiceBase.ts
+++ src/shared/api/ApiServiceBase.ts
@@ -1,52 +1,54 @@
-import {IRequestOptions} from "../types/IRequestOptions";
+import { IRequestOptions } from "../types/IRequestOptions";
 
 export class ApiServiceBase {
   API_URL = "http://localhost:5109" as const;
 
   constructor() {}
 
-  protected async get(path: string, {
-    headers
-  }: IRequestOptions = {}) {
+  protected async get(path: string, { headers }: IRequestOptions = {}) {
     try {
       const response = await fetch(`${this.API_URL}/${path}`, {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           ...headers,
         },
       });
 
       if (!response.ok) {
-        throw new Error('Failed to fetch data');
+        throw new Error("Failed to fetch data");
       }
 
       return await response.json();
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
       throw error;
     }
   }
 
   // TODO: generic
-  protected async post(url: string, body: BodyInit | null | undefined, { headers }: IRequestOptions = {}) {
+  protected async post(
+    url: string,
+    body: BodyInit | null | undefined,
+    { headers }: IRequestOptions = {},
+  ) {
     try {
       const response = await fetch(`${this.API_URL}/${url}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           ...headers,
         },
         body,
       });
 
       if (!response.ok) {
-        throw new Error('Failed to post data');
+        throw new Error("Failed to post data");
       }
 
       return await response.json();
     } catch (error) {
-      console.error('Error posting data:', error);
+      console.error("Error posting data:", error);
       throw error;
     }
   }
