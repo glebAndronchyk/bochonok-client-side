c50243a5341224bda98c79cba3f7c6cba4b4aa9c
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-05-12 23:24:55 +0300

    feat: changed position of category selector

--- src/entities/products-list/ui/ListhHeader/ListHeader.tsx
+++ src/entities/products-list/ui/ListhHeader/ListHeader.tsx
@@ -3,7 +3,7 @@ import { IProductListHeaderProps } from "../../types/IProductListHeaderProps";
 
 export const ListHeader = ({ activeCategory }: IProductListHeaderProps) => {
   return (
-    <div className="flex gap-2 items-center mb-4">
+    <div className="flex justify-between items-center mb-4">
       <h4 className="font-semibold text-xl">Products</h4>
       <CategorySelector activeCategory={activeCategory} />
     </div>
