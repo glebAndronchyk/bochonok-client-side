b7fc9848f18294fca817b01e0f84b9f7d73755e4
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-05-13 23:22:22 +0300

    feat: cards ui

--- src/entities/products-list/ui/ListComponent/ListComponent.tsx
+++ src/entities/products-list/ui/ListComponent/ListComponent.tsx
@@ -4,7 +4,8 @@ import { productsService } from "../../../../shared/api/ProductsService";
 import { EmptyList } from "../../../../shared/ui/EmptyList/EmptyList";
 import { observer } from "mobx-react";
 import { classNames } from "../../../../shared/lib/helpers/classNames";
-import { ProductCard } from "../../../../shared/ui";
+import { Button, ProductCard } from "../../../../shared/ui";
+import { useNavigate } from "react-router-dom";
 
 interface IListComponentProps {
   activeCategory: string | null;
@@ -14,6 +15,7 @@ interface IListComponentProps {
 export const ListComponent = observer(
   ({ activeCategory }: IListComponentProps) => {
     const { products, modal } = useRootState();
+    const navigate = useNavigate();
 
     useEffect(() => {
       productsService.getProductsList(activeCategory).then((productsList) => {
@@ -28,24 +30,47 @@ export const ListComponent = observer(
     };
 
     return (
-      <div
-        className={classNames(
-          "min-h-[65vh]",
-          isWithProducts
-            ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
-            : "flex items-center justify-center",
+      <div className="min-h-[65vh] flex flex-col gap-4 justify-between">
+        <div
+          className={classNames(
+            "min-h-full flex-1",
+            isWithProducts
+              ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+              : "flex items-center justify-center",
+          )}
+        >
+          {!products.simplifiedProducts.length ? (
+            <EmptyList
+              emptyText="No products in this category."
+              addNewText="Add new :D."
+              onAddNew={onAddNewProduct}
+            />
+          ) : (
+            <>
+              {products.simplifiedProducts.map((product) => {
+                const handleProductClick = () =>
+                  navigate(`product/${product.id}`);
+
+                return (
+                  <ProductCard
+                    key={product.id}
+                    onClick={handleProductClick}
+                    {...product}
+                  />
+                );
+              })}
+            </>
+          )}
+        </div>
+        {products.simplifiedProducts.length && (
+          <Button
+            variants={["dark", "md"]}
+            className="w-full mt-4"
+            onClick={onAddNewProduct}
+          >
+            Add New
+          </Button>
         )}
-      >
-        {!products.simplifiedProducts.length && (
-          <EmptyList
-            emptyText="No products in this category."
-            addNewText="Add new :D."
-            onAddNew={onAddNewProduct}
-          />
-        )}
-        {products.simplifiedProducts.map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
       </div>
     );
   },
--- src/shared/ui/ProductCard/ProductCard.tsx
+++ src/shared/ui/ProductCard/ProductCard.tsx
@@ -1,6 +1,12 @@
-import { ISimplifiedProduct } from "../../types/api/product";
+import { PointerEvent } from "react";
 
-interface IProductCardProps extends ISimplifiedProduct {}
+import { ISimplifiedProduct } from "../../types/api/product";
+import { ShoppingCartIcon, StarIcon } from "@heroicons/react/20/solid";
+import { Button } from "../Button/Button";
+
+interface IProductCardProps extends Omit<ISimplifiedProduct, "id"> {
+  onClick: () => void;
+}
 
 export const ProductCard = ({
   title,
@@ -9,18 +15,51 @@ export const ProductCard = ({
   price,
   imageB64,
   soldBy,
+  onClick,
 }: IProductCardProps) => {
+  const onAddToCart = (e: PointerEvent) => {
+    console.log(e);
+  };
+
   return (
-    <div className="p-4 bg-white shadow-xl rounded-md min-h-[25rem] max-h-[25rem] lg:min-h-[30rem] lg:max-h-[30rem] flex flex-col gap-2">
-      <div className="w-full rounded-md overflow-hidden relative flex-[4]">
-        <div className="absolute">{rating}</div>
-        <img src={imageB64} className="object-cover" alt="product-image" />
+    <div
+      onClick={onClick}
+      className="p-4 h-[27rem] transition-all active:scale-[102%] hover:scale-[102%] bg-white shadow-xl rounded-md flex flex-col gap-6 cursor-pointer"
+    >
+      <div className="w-full rounded-md overflow-hidden flex-[3] relative bg-gray-200">
+        <div className="absolute top-0 right-0 bg-yellow-200 items-center rounded-md p-2 text-lg flex gap-2 z-20">
+          <span className="font-semibold">{rating}</span>
+          <StarIcon className="relative -top-[0.10rem] text-yellow-500 w-6 h-6" />
+        </div>
+        <img
+          src={imageB64}
+          className="block object-contain p-3 absolute rounded-md  w-full h-full"
+          alt="product-image"
+        />
       </div>
-      <div className="flex-1">
-        <p>{title}</p>
-        <p>{description}</p>
-        <p>{price}</p>
-        <p>{soldBy}</p>
+      <div className="flex-1 flex justify-between">
+        <div>
+          <p className="text-2xl font-semibold pb-1 mb-1 border-b-2 border-b-green-500">
+            {title}
+          </p>
+          <p className="text-gray-500 text-md">{description}</p>
+          <p className="text-gray-500 text-sm">Sold by: {soldBy}</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center self-stretch">
+          <p>
+            <span className="text-green-500 text-3xl font-semibold">
+              {price}
+            </span>{" "}
+            <span className="text-gray-500 text-xl font-medium">hrn.</span>
+          </p>
+          <Button
+            variants={["dark", "md"]}
+            className="p-2"
+            onPointerDown={onAddToCart}
+          >
+            <ShoppingCartIcon className="w-6 h-6" />
+          </Button>
+        </div>
       </div>
     </div>
   );
